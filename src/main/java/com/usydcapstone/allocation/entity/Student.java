119551e1c5e7591e748d282b60bc985d9243dd0a
79192 <xuch970506@gmail.com>
2021-05-12 17:43:39 +0800

    Merge pull request #12 from JJJustCoding/feature-group-join-student
    Add group left join student

--- src/main/java/com/usydcapstone/allocation/entity/Student.java
+++ src/main/java/com/usydcapstone/allocation/entity/Student.java
@@ -1,11 +1,23 @@
 package com.usydcapstone.allocation.entity;
 
 
-import lombok.Data;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.*;
+
+import java.io.Serializable;
+
 
 @Data
-public class Student {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Student implements Serializable {
+    private static final long serialVersionUID =  -5144055068797033748L;
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
     private String id;
+    private String sid;
     private String name;
     private String email;
     private String password;
